07b33dd20e18cf63ec412c3168f7f567381d21e4
GabrielL <79658132+gluna1203@users.noreply.github.com>
2022-01-26 15:02:43 -0700

    Installation

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+const expressSession = require('express-session');
+const routes = require('./routes/routes.js');
+const port = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + "/views");
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(expressSession({
+    secret: 'wh4t3v3r',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next();
+});
+
+const urlencodedParser = express.urlencoded({
+    extended: false
+})
+
+const checkAuth = (req, res, next) => {
+    if (req.session.user && req.session.user.isAuthenticated) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
