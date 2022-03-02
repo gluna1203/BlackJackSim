9d2840fa7b12fd824407019fca7d259e7db3445a
GabrielL <79658132+gluna1203@users.noreply.github.com>
2022-03-02 14:35:13 -0700

    Cards update

--- public/cards.js
+++ public/cards.js
@@ -11,7 +11,7 @@ function card(value, name, suit) {
 }
 
 function deck() {
-    this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    this.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
     var cards = [];
 
@@ -22,6 +22,50 @@ function deck() {
     }
 
     return cards;
+}
+
+function calculateHandValue(Array){
+    var TPV = 0;
+    var hasAce = 1;
+    for (var i = 0;i<Array.length;i++){
+        if(Array[i].startwWith("2")){
+            TPV = TPV + 2;
+        }
+        if(Array[i].startsWith("3")){
+            TPV = TPV + 3;
+        }
+        if(Array[i].startsWith("4")){
+            TPV = TPV + 4;
+        }
+        if(Array[i].startsWith("5")){
+            TPV = TPV + 5;
+        }
+        if(Array[i].startsWith("6")){
+            TPV = TPV + 6;
+        }
+        if(Array[i].startsWith("7")){
+            TPV = TPV + 7;
+        }
+        if(Array[i].startsWith("8")){
+            TPV = TPV + 8;
+        }
+        if(Array[i].startsWith("9")){
+            TPV = TPV + 9;
+        }
+        if(Array[i].startsWith("10" || "J" || "Q" || "K")){
+            TPV = TPV + 10;
+        }
+        if(Array[i].startsWith("A")){
+            TPV = TPV + 11;
+            hasAce++;
+        } 
+    }
+    for(var i = 0; i<hasAce; i++){
+        if(TPV > 21){
+            TPV -= 10;
+        }
+    }
+    return TPV;
 }
 
 
@@ -40,6 +84,10 @@ const shuffleArr = myArr => {
 var playerHand = [];
 var dealerHand = [];
 var deckHand = shuffleArr(deck());
+
+var DealerPoints = calculateHandValue(dealerHand);
+
+var PlayerPoints = calculateHandValue(playerHand);
 
 const handleClick = evt => {
     //console.log(evt.target.id);
@@ -64,18 +112,40 @@ const handleClick = evt => {
             deckHand.pop(cardFour);
 
             console.log(deckHand)
-            console.log(playerHand)
-            console.log(dealerHand)
+            console.log("Player: " + PlayerPoints)
+            console.log("Dealer: " + DealerPoints)
             break;
         case 'hit':
             console.log('Someone has clicked Button hit!')
             var cardPlayed = Math.floor(Math.random() * (deckHand.length + 1));
             playerHand.push(deckHand[cardPlayed])
             deckHand.pop(cardPlayed)
+            console.log(PlayerPoints)
             console.log(playerHand)
             break;
         case 'play':
             console.log('Someone has clicked Button play!')
+            if (calculateHandValue(dealerHand) < 10){
+                var cardPlayed = Math.floor(Math.random() * (deckHand.length + 1));
+                dealerHand.push(deckHand[cardPlayed])
+                deckHand.pop(cardPlayed)
+            }
+
+            if(PlayerPoints == 21){
+                PlayerWon();
+                break;
+            } else if(PlayerPoints > 21){
+                DealerHand();
+                break;
+            }
+
+            if(DealerPoints == 21){
+                DealerWon();
+                break;
+            }else if(DealerPoints > 21){
+                PlayerWon();
+                break;
+            }
             break;
     }
 }
